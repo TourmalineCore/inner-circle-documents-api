30a34bca9b07e9b4c5fcc5ed1836d292c5b2e8b3
Dmitriy Myakotin <demon4301@gmail.com>
2024-03-04 11:14:01 +0500

    feat: #8693yhxv7: change send mailing payslips service

--- Core/MailPayslipsModel.cs
+++ Core/MailPayslipsModel.cs
@@ -6,12 +6,14 @@ public class MailPayslipsModel
 {
     public string To { get; set; }
     public string Subject { get; set; }
+    public string Body { get; set; }
     public IFormFile File { get; set; }
 
-    public MailPayslipsModel(string to, string subject, IFormFile file)
+    public MailPayslipsModel(string to, string subject, string body, IFormFile file)
     {
         To = to;
         Subject = subject;
+        Body = body;
         File = file;
     }
 }
