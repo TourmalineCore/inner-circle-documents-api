e4738ec9a34ee6d44a76e15d3b92ba130a6004f5
Anna <141008173+Annushkaaaaa@users.noreply.github.com>
2024-11-15 16:01:51 +0500

    feat: cleanup code and codestyle (#23)
    * feat: add configurations
    
    * feat: add configuration description
    
    * ci: configure launchsettings
    
    * ci: configure docker compose and urls
    
    * ci: configure ports
    
    * ref: cleanup docker compose

--- Application/Services/Options/InnerCircleServiceUrl.cs
+++ Application/Services/Options/InnerCircleServiceUrl.cs
@@ -2,7 +2,7 @@ namespace Application.Services.Options;
 
 public class InnerCircleServiceUrls
 {
-    public string SalaryServiceUrl { get; set; }
+    public string EmployeesServiceUrl { get; set; }
 
     public string EmailSenderServiceUrl { get; set; }
 }
