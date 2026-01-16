4bd1f1ee4f8d397bcf651784db73f3c051cca2b4
Dmitriy Myakotin <75628188+MDI74@users.noreply.github.com>
2026-01-16 09:09:25 +0500

    Revert "fix: #35: rename EmailSenderServiceUr to EmailSenderApiRootUrl and EmployeesServiceUrl to EmployeesApiRootUrl"
    This reverts commit 5ec89a6840158e7816abd11b87c2eaaf5a0bb2ed.

--- Application/Services/Options/InnerCircleServiceUrl.cs
+++ Application/Services/Options/InnerCircleServiceUrl.cs
@@ -2,7 +2,7 @@ namespace Application.Services.Options;
 
 public class InnerCircleServiceUrls
 {
-  public string EmployeesApiRootUrl { get; set; }
+  public string EmployeesServiceUrl { get; set; }
 
-  public string EmailSenderApiRootUrl { get; set; }
+  public string EmailSenderServiceUrl { get; set; }
 }
